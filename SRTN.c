bafbc4163883828db355e1ada9214b678147799a
akaFTS <gustavo84171@hotmail.com>
2016-09-07 20:39:08 -0300

    Deixando a coisa mais clara no SRTN

--- SRTN.c
+++ SRTN.c
@@ -23,9 +23,12 @@ static void sortByRemaining (PROCESS *ini, PROCESS *new) {
     PROCESS *p, *temp;
 
     p = ini;
+    /*Vamos procurar o ultimo processo da lista que tenha menos rem
+      que o processo que quer entrar */
     while (p->next != NULL && p->next->rem <= new->rem) 
         p = p->next;
     
+    /* Adicionamos ele logo em seguida */
     temp = p->next;
     p->next = new;
     new->next = temp;
@@ -33,13 +36,16 @@ static void sortByRemaining (PROCESS *ini, PROCESS *new) {
 
 void SRTN (FILE *out, char *d) { 
     struct timespec t_ini;  
-    int context = 0, count = 0; 
+    int context = 0, traceline = 0; 
+    float dt;
     PARAMS *args;                      
-    PROCESS *p, *q, *temp, *ready, *older;
+    PROCESS *p, *q, *temp, *ready, *old, *running;
 
-    /* Cabeça da lista de processos prontos.                         */
+    /* Cabeça da lista de processos prontos.  (é uma cabeça vazia)   
+       O primeiro elemento da lista é o que vai estar rodando.      */
     ready = malloc (sizeof (PROCESS));
     ready->next = NULL;
+    running = ready->next;
     p = head->next;
 
     /* Inicio da contagem de tempo.                                  */
@@ -49,79 +55,86 @@ void SRTN (FILE *out, char *d) {
     /*e não houverem processos querendo entrar no sistema (p)        */
     while (p != NULL || ready->next != NULL) {
 
-        /* Se houver processo querendo entrar no sistema, gera uma  */
-        /*preempção.                                                */
-        if (p != NULL && p->t0 <= check_timer (t_ini)) {
+        /* Se tiver um processo p querendo entrar neste momento */
+        dt = check_timer(t_ini);
+        if (p != NULL && p->t0 <= dt) {
+
             if (d != NULL)
                 printLog (PROC_ARRIVAL, p->name, p->line);
 
-            /* Desabilita a thread que está ativa. */
-            if (ready->next && ready->next->canRun)
-                ready->next->canRun = FALSE;
+            /* Desabilita a thread ativa. */
+            if (running && running->canRun)
+                running->canRun = FALSE;
             
-            older = ready->next;
-            /* Primeiro processo que ainda não chegou no*/
-            /*sistema.                                  */
-            q = p->next; 
+            old = running;
 
-            /* Ordena a fila apontada por ready baseado no remaining */
-            /*time, inserindo os processos que chegaram nesse instan-*/
-            /*te.                                                    */
-            sortByRemaining (ready, p);
-
-            while (q != NULL && p->t0 == q->t0) {
-                temp = q->next;
-                sortByRemaining (ready, q);
-                q = temp;
+            /* guardamos a referencia ao proximo da fila
+               e então inserimos p e todos os outros  que entraram
+               junto na fila de execução ordenados pelo seu tempo
+               remanescente.                                       */
+            while (p != NULL && p->t0 <= dt) {
+                temp = p->next;
+                sortByRemaining (ready, p);
+                p = temp;
             }
+            /* Ao fim desse while, p já está apontando para o proximo
+               processo que vai entrar, daqui a alguns momentos    */
+
+
+            /* Depois dessa reorganizaçao, o primeiro da fila ready
+               é quem vai ser executado.                           */
+            running = ready->next;
+
 
             /* Cria uma thread nova se o próximo processo ainda não*/
             /*começou a ser executado.                             */
-            if (ready->next->id == -1) {
+            if (running->id == -1) {
                 args = malloc (sizeof (PARAMS));
-                args->p     = ready->next;
-                pthread_create (&ready->next->id, NULL, &func, args);
+                args->p     = running;
+                pthread_create (&running->id, NULL, &func, args);
             }
             /* Libera a thread.                                    */
-            ready->next->canRun = TRUE;
+            running->canRun = TRUE;
 
             /* Verifica se houve troca de contexto.                */
-            if (older != ready->next) {
-                if (older) {
+            if (old != running) {
+                if (old) {
                     printLog (CPU_OUT, older->name, 0);
                     context++;
                 }
                 printLog (CPU_ENTER, ready->next->name, 0);
             }
-
-            /* p agora é o próximo processo que quer entrar        */
-            p = q;
         }
 
-        /* Se o próximo processo já terminou de executar. */
-        if (ready->next != NULL && ready->next->rem < 1e-10) {
-            pthread_join (ready->next->id, NULL);
+        /* Se o processo que está rodando terminou de executar. */
+        if (running != NULL && running->rem < 1e-10) {
+            pthread_join (running->id, NULL);
 
+            /* Retiramos ele da CPU, printamos o numero da linha
+               dele no trace, e incrementamos esse valor.       */
             if (d != NULL) {
-                printLog (CPU_OUT, ready->next->name, 0);
-                printLog (PROC_END, ready->next->name, count++);
+                printLog (CPU_OUT, running->name, 0);
+                printLog (PROC_END, running->name, traceline++);
             }
 
             /* Imprime na saida o resultado.                */
             fprintf(out, "%s %.3f (deadline: %.3f)\n", ready->next->name, check_timer (t_ini), ready->next->deadline);
 
-            /* Avança para o próximo processo e, se existir,*/
-            /*libera-o.                                     */
-            temp = ready->next;
-            ready->next = ready->next->next;
-            if (ready->next != NULL) {
-                if (ready->next->id == -1) {
+            /* Avança para o próximo processo na fila ready */
+            temp = running;
+            running = running->next;
+
+            /* Verificamos se ele existe. Pode ser que nao tenha ninguem
+               na fila, e a CPU fica ociosa.                           */
+            if (running != NULL) {
+                /* Cria a thread do proximo processo, se necessario.   */
+                if (running->id == -1) {
                     args = malloc (sizeof (PARAMS));
-                    args->p     = ready->next;
-                    pthread_create (&ready->next->id, NULL, &func, args);
+                    args->p     = running;
+                    pthread_create (&running->id, NULL, &func, args);
                 }
-                ready->next->canRun = TRUE;
-                printLog (CPU_ENTER, ready->next->name, 0);
+                running->canRun = TRUE;
+                printLog (CPU_ENTER, running->name, 0);
             }
             free (temp);
         }
