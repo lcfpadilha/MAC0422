9d1e0259736507b9cb1e54d09e1e2043e0502263
Cloud User <centos@ip-172-31-13-70.sa-east-1.compute.internal>
2016-09-08 20:08:19 +0000

    Mais um tapa nos scripts

--- run2.sh
+++ run2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 for i in `seq 1 30`; do
-    ep1 2 traces/pequeno/arq$i.in results/pequeno/SRTN/arq$i.out y > results/pequeno/SRTN/stdout$i.txt 2> results/pequeno/SRTN/err$i.txt
+    ./ep1 2 traces/pequeno/arq$i.in results/pequeno/SRTN/arq$i.out y > results/pequeno/SRTN/stdout$i.txt 2> results/pequeno/SRTN/err$i.txt
 done 
 
 for i in `seq 1 30`; do
-    ep1 2 traces/medio/arq$i.in results/medio/SRTN/arq$i.out y > results/medio/SRTN/stdout$i.txt 2> results/medio/SRTN/err$i.txt
+    ./ep1 2 traces/medio/arq$i.in results/medio/SRTN/arq$i.out y > results/medio/SRTN/stdout$i.txt 2> results/medio/SRTN/err$i.txt
 done 
 
 for i in `seq 1 30`; do
-    ep1 2 traces/grande/arq$i.in results/grande/SRTN/arq$i.out y > results/grande/SRTN/stdout$i.txt 2> results/grande/SRTN/err$i.txt
+    ./ep1 2 traces/grande/arq$i.in results/grande/SRTN/arq$i.out y > results/grande/SRTN/stdout$i.txt 2> results/grande/SRTN/err$i.txt
 done 
--- run3.sh
+++ run3.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 for i in `seq 1 30`; do
-    ep1 3 traces/pequeno/arq$i.in results/pequeno/MFBQ/arq$i.out y > results/pequeno/MFBQ/stdout$i.txt 2> results/pequeno/MFBQ/err$i.txt
+    ./ep1 3 traces/pequeno/arq$i.in results/pequeno/MFBQ/arq$i.out y > results/pequeno/MFBQ/stdout$i.txt 2> results/pequeno/MFBQ/err$i.txt
 done 
 
 for i in `seq 1 30`; do
-    ep1 3 traces/medio/arq$i.in results/medio/MFBQ/arq$i.out y > results/medio/MFBQ/stdout$i.txt 2> results/medio/MFBQ/err$i.txt
+    ./ep1 3 traces/medio/arq$i.in results/medio/MFBQ/arq$i.out y > results/medio/MFBQ/stdout$i.txt 2> results/medio/MFBQ/err$i.txt
 done 
 
 for i in `seq 1 30`; do
-    ep1 3 traces/grande/arq$i.in results/grande/MFBQ/arq$i.out y > results/grande/MFBQ/stdout$i.txt 2> results/grande/MFBQ/err$i.txt
+    ./ep1 3 traces/grande/arq$i.in results/grande/MFBQ/arq$i.out y > results/grande/MFBQ/stdout$i.txt 2> results/grande/MFBQ/err$i.txt
 done 
