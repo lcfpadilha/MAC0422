32c65035efc46382701bfd3eb6cecfcdfe6ca2ac
Cloud User <centos@ip-172-31-13-70.sa-east-1.compute.internal>
2016-09-08 19:47:25 +0000

    Ajeitando scripts de teste

--- run1.sh
+++ run1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 for i in `seq 1 30`; do
-    ep1 1 traces/pequeno/arq$i.in results/pequeno/FCFS/arq$i.out y > results/pequeno/FCFS/stdout$i.txt 2> results/pequeno/FCFS/err$i.txt && results/pequeno/FCFS/stdout$i.txt
+    ./ep1 1 traces/pequeno/arq$i.in results/pequeno/FCFS/arq$i.out y > results/pequeno/FCFS/stdout$i.txt 2> results/pequeno/FCFS/err$i.txt
 done 
 
 for i in `seq 1 30`; do
-    ep1 1 traces/medio/arq$i.in results/medio/FCFS/arq$i.out y > results/medio/FCFS/stdout$i.txt 2> results/medio/FCFS/err$i.txt && cat results/medio/FCFS/stdout$i.txt
+    ./ep1 1 traces/medio/arq$i.in results/medio/FCFS/arq$i.out y > results/medio/FCFS/stdout$i.txt 2> results/medio/FCFS/err$i.txt
 done 
 
 for i in `seq 1 30`; do
-    ep1 1 traces/grande/arq$i.in results/grande/FCFS/arq$i.out y > results/grande/FCFS/stdout$i.txt 2> results/grande/FCFS/err$i.txt && cat results/grande/FCFS/stdout$i.txt
+    ./ep1 1 traces/grande/arq$i.in results/grande/FCFS/arq$i.out y > results/grande/FCFS/stdout$i.txt 2> results/grande/FCFS/err$i.txt
 done 
